497ab04933fc3fc57349357ecb0a8eddf77e9b81
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-02-16 21:12:02 +0700

    STYLIX : Add stylix manager

--- home/zed-editor/default.nix
+++ home/zed-editor/default.nix
@@ -3,11 +3,6 @@
     enable = true;
     extensions = [ "nix" "toml" "rust" ];
     userSettings = {
-      theme = {
-        mode = "system";
-        dark = "One Dark";
-        light = "One Light";
-      };
       hour_format = "hour24";
       vim_mode = true;
     };

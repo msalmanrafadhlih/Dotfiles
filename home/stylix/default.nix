1266e21f0d45e437e3634f26c4124d8810d0f630
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-02-16 21:46:26 +0700

    STYLIX : Add stylix manager

--- home/stylix/default.nix
+++ home/stylix/default.nix
@@ -19,7 +19,7 @@
     # a warning will be printed when the user configuration is being built.    
     enableReleaseChecks = true;
 
-    polarity = "either"; # "either","dark","light"
+    polarity = "dark"; # "either","dark","light"
 
     imageScalingMode = "fill"; # stretch, fill, fit, center, tile
     image = pkgs.fetchurl {

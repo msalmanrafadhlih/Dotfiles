5c906b15c0a06ee5e20ad35b6e3b74929d3b7ba2
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-03-17 05:04:18 +0700

    refactor(users): reorganize user configurations by window manager
    Replace legacy user profiles (dev, gaming, server, work) with window
    manager specific modules (bspwm, hyprland, niri) for better
    configuration granularity.

---
+++ users/niri/default.nix
@@ -0,0 +1,37 @@
+# exported to ../../flake.nix
+{ lib, pkgs, config, ... }:
+
+let
+  dotfiles = "bspwm";
+  branch = "main";
+  home = config.home.homeDirectory;
+  dotfiles_path = "${home}/.dotfiles/${dotfiles}";
+  repo_url = "https://github.com/msalmanrafadhlih/racooonfig.git";
+in
+
+{
+  home.activation = {
+   	setupDotfiles = lib.hm.dag.entryAfter ["writeBoundary"] ''
+		  # Buat folder .dotfiles jika belum ada
+		  mkdir -p ${dotfiles_path}
+
+		  if [ ! -d "${dotfiles_path}/.git" ]; then
+				echo "--- Cloning Dotfiles dari GitHub (${branch}) ---"
+				${pkgs.git}/bin/git clone -b ${branch} --single-branch --depth 1 ${repo_url} "${dotfiles_path}"
+		  else
+				echo "--- Dotfiles sudah ada, melakukan check update (safe) ---"
+				cd "${dotfiles_path}"
+				# Melakukan fetch saja agar tidak merusak kerjaan lokal
+				${pkgs.git}/bin/git fetch origin ${branch}
+				echo "Gunakan 'git pull --rebase' secara manual di folder dotfiles jika ingin sinkronisasi."
+		  fi
+		'';
+   };
+
+  imports = [
+    ./packages.nix
+    ./configs.nix
+    ./xdg-portal.nix
+    ../users.nix
+  ];
+}
